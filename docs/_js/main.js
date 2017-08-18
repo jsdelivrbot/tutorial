27e649e96222fd1e4df8386a940c167f4aade31c
Wolfgang <wgehner@gmail.com>
2017-08-18 09:53:27 -0700

    Add transitions

--- docs/_js/main.js
+++ docs/_js/main.js
@@ -1,96 +1,84 @@
-'use strict'
-loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
-	success: function(){
+var TM = {
 
-		/* loadjs([
+	loadLibs: function(){
+		console.log('loadLibs called')
 
-			'./_js/vendor/jquery.jsForm.min.js'
-			,'./_js/vendor/jquery.fullpage.min.js'
-			,'./_js/vendor/jquery.fullpage.min.css'
-
-			], { success: function(){
-				libsLoaded()
-			}
-		})//loadjs */
-		libsLoaded()
-
-	}//suc
-})
-
-function toggleSub(toggleEl, show) {
-	var wrapperEl = toggleEl.parentNode,
-	menuEl = toggleEl.nextElementSibling
-	
-	function closeDropdownFn() { 
-		menuEl.classList.remove('mui--is-open') 
+		return Promise.all([
+			TS.load('/docs/_js/submenu.js') //page transitions
+			, TS.load('//cdn.jsdelivr.net/jquery.transit/0.9.12/jquery.transit.min.js')
+			//, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/transit/topseed-transitions-1.1.js') //page transitions
+			, TS.load('/docs/_js/topseed-transitions-1.2.js') //page transitions
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-4.3.js')
+		])
+		.then(TM.libsLoaded)
 	}
 
-	function openDropdownFn(){
-		var wrapperRect = wrapperEl.getBoundingClientRect(),
-		toggleRect = toggleEl.getBoundingClientRect()
-		var top = toggleRect.top - wrapperRect.top + toggleRect.height
-		var left =  toggleRect.left + toggleRect.width - menuEl.width
-		menuEl.style.top = top + 'px'
-		if (menuEl.classList.contains('mui-dropdown__menu--right'))
-			menuEl.style.left = left + 'px'
-		else
-			menuEl.style.left = wrapperRect.left + 'px'
+	, libsLoaded: function(){
 		
-		menuEl.classList.add('mui--is-open')
+		TS.signalAppReady()
+
+		TT.ScontentID = '#content-wrapper'
+		TT.handle(function(evt) {
+			if (TT.PRE == evt.typ) {
+				//example transitions out:
+				/*var newArr = evt.toHref.split('/').filter(Boolean)
+				var newInd = Number(newArr[newArr.length-1].split('-')[0])
+				var oldArr = evt.fromHref.split('/').filter(Boolean)
+				var oldInd = Number(oldArr[oldArr.length-1].split('-')[0])
+				console.log('oldInd'+oldInd+'/newInd'+newInd)
+
+				if (newInd > oldInd) {
+				{
+					TR.boxOut('#content-wrapper', evt, 300, .87, 36, '#303f9f')
+				}*/
+
+
+				/*if (evt.fromHref.indexOf('/speakers/')==-1 && !evt.back) {	
+					//TR.fadeOut('#content-wrapper', evt, 50, 0)
+					TR.boxOut('#content-wrapper', evt, 300, .87, 36, '#303f9f')
+				}*/
+			}
+			if (TT.PAGE === evt.typ) {
+				$('#appbar').removeClass('appbar-hide') //reset appbar visibility
+				$('#appbar').addClass('appbar-show')
+
+				//example transitions in:
+				//TR.show('#content-wrapper', evt) //no transition, same as $('#content-wrapper').html(evt.$new)
+				//TR.fadeIn('#content-wrapper', evt, 150)
+				console.log('oldUrl'+(evt.fromHref)+'/newUrl'+(evt.toHref))
+				var newArr = evt.toHref.split('/').filter(Boolean)
+				var newInd = Number(newArr[newArr.length-1].split('-')[0])
+				var oldArr = evt.fromHref.split('/').filter(Boolean)
+				var oldInd = Number(oldArr[oldArr.length-1].split('-')[0])
+				console.log('oldInd'+oldInd+'/newInd'+newInd)
+
+				if (newInd < oldInd) { 
+
+					console.log('newInd'+newInd+'<oldInd'+oldInd)
+
+					TR.uncoverDown('#content-wrapper', evt, 350)
+					//TR.uncoverUp('#content-wrapper', evt, 350)
+					//TR.uncoverRight('#content-wrapper', evt, 350)
+					//TR.uncoverLeft('#content-wrapper', evt, 350)
+				}
+				else {
+
+					if (evt.toHref.indexOf('agenda')>-1) { //going to agenda
+						TR.splitVerticalOut('#content-wrapper', evt, 350)
+					}
+					else { //default
+						TR.coverUp('#content-wrapper', evt, 350, 35)
+						//TR.coverUp('#content-wrapper', evt, 350)
+						//TR.coverRight('#content-wrapper', evt, 350)
+						//TR.coverLeft('#content-wrapper', evt, 350)
+					}
+				}
+				$('html, body').scrollTop(0)
+			}
+		})
 	}
-	show ? openDropdownFn() : closeDropdownFn()
-}
 
-var submenuInitialized = false
+} //class
 
-function initSubmenu(){
-
-	console.log('initSubmenu')
-
-	//if (submenuInitialized) return
-	submenuInitialized = true
-
-	$('.submenu').hover(function() {
-			toggleSub(this.firstChild, true)
-		},
-		function() {
-			toggleSub(this.firstChild, false)
-	})
-
-	$('.submenu > a').attr('aria-haspopup','true') //IE, Edge
-
-	$('.submenu > a').click(function() {
-			toggleSub(this, true)
-	})
-
-	$('.submenu > ul').click(function() {
-			toggleSub(this.previousElementSibling, false)
-	})
-
-}	
-
-
-function libsLoaded(){
-	
-	TS.signalAppReady()
-	initSubmenu() 
-
-	TT.ScontentID ='#content-wrapper'
-	TT.handle(function(evt) {
-		//console.log(':')
-		if(TT.PRE==evt.typ)  {//start
-			//console.log(evt.$new)
-			//$('#content-wrapper').fadeTo(100,.2)
-		}
-		if(TT.PAGE==evt.typ)  {//new pg loaded
-			console.log('TT')
-
-			$(TT.ScontentID).html(evt.$new)
-			//$('#content-wrapper').fadeTo(100,1)
-			
-			$('html, body').scrollTop(0)
-		}
-	})
-}
-
+TM.loadLibs()
 
---
+++ docs/_js/submenu.js
@@ -0,0 +1,50 @@
+function toggleSub(toggleEl, show) {
+	var wrapperEl = toggleEl.parentNode,
+	menuEl = toggleEl.nextElementSibling
+	
+	function closeDropdownFn() { 
+		menuEl.classList.remove('mui--is-open') 
+	}
+
+	function openDropdownFn(){
+		var wrapperRect = wrapperEl.getBoundingClientRect(),
+		toggleRect = toggleEl.getBoundingClientRect()
+		var top = toggleRect.top - wrapperRect.top + toggleRect.height
+		var left =  toggleRect.left + toggleRect.width - menuEl.width
+		menuEl.style.top = top + 'px'
+		if (menuEl.classList.contains('mui-dropdown__menu--right'))
+			menuEl.style.left = left + 'px'
+		else
+			menuEl.style.left = wrapperRect.left + 'px'
+		
+		menuEl.classList.add('mui--is-open')
+	}
+	show ? openDropdownFn() : closeDropdownFn()
+}
+
+var submenuInitialized = false
+
+function initSubmenu(){
+
+	console.log('initSubmenu')
+
+	//if (submenuInitialized) return
+	submenuInitialized = true
+
+	$('.submenu').hover(function() {
+			toggleSub(this.firstChild, true)
+		},
+		function() {
+			toggleSub(this.firstChild, false)
+	})
+
+	$('.submenu > a').attr('aria-haspopup','true') //IE, Edge
+
+	$('.submenu > a').click(function() {
+			toggleSub(this, true)
+	})
+
+	$('.submenu > ul').click(function() {
+			toggleSub(this.previousElementSibling, false)
+	})
+}
